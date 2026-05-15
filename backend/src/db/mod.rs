dc44cdc076e3ab7dd98327f99d3fc700aa9906b3
Oscar Rompich <oscarligo6@gmail.com>
2026-05-15 01:24:15 -0600

    refactor: update projects structure

--- backend/src/db/mod.rs
+++
@@ -1,7 +0,0 @@
-pub mod product_repository;
-pub mod supplier_repository;
-pub mod category_repository_impl;
-pub mod client_repository_impl;
-pub mod employee_repository;
-pub mod report_repository;
-pub mod sale_repository;
