216dc7115f7b3d5bf673e1d1a56a98b0c289f73c
Oscar Rompich <oscarligo6@gmail.com>
2026-05-15 01:06:26 -0600

    refactor: new project's structure

--- backend/src/handlers/category_handler.rs
+++ backend/src/handlers/category_handler.rs
@@ -1,11 +1,11 @@
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
 
-use crate::db::category_repository;
+use crate::db::category_repository_impl;
 use crate::error::{map_sqlx_error, ApiError};
 
 pub async fn list_categories_handler(pool: web::Data<PgPool>) -> Result<HttpResponse, ApiError> {
-    let categories = category_repository::list_categories(&pool)
+    let categories = category_repository_impl::list_categories(&pool)
         .await
         .map_err(map_sqlx_error)?;
 
--- backend/src/handlers/client_handler.rs
+++ backend/src/handlers/client_handler.rs
@@ -1,11 +1,11 @@
 use actix_web::{web, HttpResponse};
 use sqlx::PgPool;
 
-use crate::db::client_repository;
+use crate::db::client_repository_impl;
 use crate::error::{map_sqlx_error, ApiError};
 
 pub async fn list_clients_handler(pool: web::Data<PgPool>) -> Result<HttpResponse, ApiError> {
-    let clients = client_repository::list_clients(&pool)
+    let clients = client_repository_impl::list_clients(&pool)
         .await
         .map_err(map_sqlx_error)?;
 
